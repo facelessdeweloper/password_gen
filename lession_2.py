44e83d8c55a335df280fc552916dc0d102dcdc77
sqwezyyy <161950094+sqwezyyyLearnDox@users.noreply.github.com>
2024-04-24 10:31:03 +0300

    Add files via upload

---
+++ lession_2.py
@@ -0,0 +1,29 @@
+# Импорт модуля random
+import random
+
+# Создаем функцию для генерации пароля
+def generate_password(length):
+    digits = '1234567890'
+    uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+    symbols = '!@#$%^&*()-+'
+
+    password = ''
+    var = [digits, uppercase_letters, lowercase_letters, symbols]
+
+    # Генерация пароля
+    if length < 12:
+        print('Ошибка! Пароль должен иметь не менее 12 символов')
+    else:
+        password += random.choice(digits)
+        password += random.choice(uppercase_letters)
+        password += random.choice(lowercase_letters)
+        password += random.choice(symbols)
+
+        while len(password) < length:
+            password += random.choice(var[random.randint(0, 3)])
+
+        print(password)
+
+# Пример вызова функции с длиной пароля 12
+generate_password(12)
